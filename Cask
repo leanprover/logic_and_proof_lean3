7f36ddccbc7031dd11040259abd2e043c2fa1a0e
Soonho Kong <soonhok@cs.cmu.edu>
2015-08-10 16:03:53 -0400

    feat: copy infrastructure from leanprover/tutorial

---
+++ Cask
@@ -0,0 +1,13 @@
+(source melpa)
+(source gnu)
+(source org)
+(development
+ (depends-on "cl-lib")
+ (depends-on "dash" 2.8.0)
+ (depends-on "dash-functional" 1.1.0)
+ (depends-on "f")
+ (depends-on "company")
+ (depends-on "flymake")
+ (depends-on "s")
+ (depends-on "org-plus-contrib")
+ (depends-on "htmlize"))
---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# 1. Build
+make
+make build_nav_data
+
+# 2. Check cssmin, minify
+hash cssmin 2>/dev/null || { echo >&2 "cssmin is not installed. Run 'npm -g i minify'."; exit 1; }
+hash minify 2>/dev/null || { echo >&2 "minify is not installed. Run 'npm -g i minify'."; exit 1; }
+
+# 3. Deploy
+mkdir deploy
+cd deploy
+rm -rf *
+git init
+cp ../*.html ../logic_and_proof.pdf ../quickref.pdf .
+cp -r ../css ../images ../fonts ../js .
+for CSS in css/*.css
+do
+    cssmin ${CSS} > ${CSS}.min
+    mv ${CSS}.min ${CSS}
+done
+for JS in js/*.js
+do
+    minify ${JS} > ${JS}.min
+    mv ${JS}.min ${JS}
+done
+git add -f *.html logic_and_proof.pdf quickref.pdf
+git add -f css/*
+git add -f images/*
+git add -f fonts/*
+git add -f js/*
+git commit -m "Update `date`"
+git push git@github.com:$1/logic_and_proof +HEAD:gh-pages
+cd ../
+rm -rf deploy
